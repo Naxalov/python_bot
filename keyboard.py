f932d151c3b32b20d4adfc85e20c5dc0b32391c1
ZARIFMAC <uzbzarif@gmail.com>
2022-08-27 10:16:04 +0500

    key tmp

---
+++ keyboard.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater,MessageHandler,Filters,CallbackContext,CommandHandler
+from telegram import Update,ReplyKeyboardMarkup,KeyboardButton
+
+import os
+TOKEN = os.environ['TOKEN']
+
+updater = Updater(TOKEN)
+
+#Handler
+def echo(update: Update, context: CallbackContext):
+    txt=update.message.text
+    user_id = update.message.from_user.id
+    bot = context.bot
+    bot.sendMessage(user_id,txt)
+    return 
+
+def start(update: Update, context: CallbackContext):
+    txt='Welcome to our echo bot'
+    user_id = update.message.from_user.id
+    bot = context.bot
+    
+    return 
+
+
+updater.dispatcher.add_handler(CommandHandler('start',start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
