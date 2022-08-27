bc8d1a5b7b5225ee274186461a83d7e265c74710
ZARIFMAC <uzbzarif@gmail.com>
2022-08-27 11:04:29 +0500

    get query

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater,MessageHandler,Filters,CallbackContext,CommandHandler
+from telegram.ext import Updater,MessageHandler,Filters,CallbackContext,CommandHandler,CallbackQueryHandler
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
 
 import os
@@ -18,14 +18,19 @@ def start(update: Update, context: CallbackContext):
     txt='Welcome to our echo bot'
     user_id = update.message.from_user.id
     bot = context.bot
-    inlineKeyboard = InlineKeyboardButton('INLINE',url='https://t.me/codeschooluz')
+    inlineKeyboard = InlineKeyboardButton('INLINE',callback_data='data 123')
+
     reply_markup = InlineKeyboardMarkup([[inlineKeyboard]])
     update.message.reply_text(txt,reply_markup=reply_markup)
     return 
 
+def getQuery(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer('DONE',show_alert=True)
+    print(query.data)
 
 updater.dispatcher.add_handler(CommandHandler('start',start))
 updater.dispatcher.add_handler(MessageHandler(Filters.text,echo))
-
+updater.dispatcher.add_handler(CallbackQueryHandler(getQuery))
 updater.start_polling()
 updater.idle()
